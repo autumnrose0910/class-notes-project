0b18e8ca5608db0b5177034384d3d47b9e23c0b5
autumn rose <122769234+autumnrose0910@users.noreply.github.com>
2026-02-18 07:32:10 -0700

    Update api.js

--- class-notes-project-/class-notes-frontend/src/api.js
+++ class-notes-project-/class-notes-frontend/src/api.js
@@ -1,3 +1,6 @@
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001"
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3001"
+    : "https://class-notes-project.onrender.com"
 
 export default API_URL
